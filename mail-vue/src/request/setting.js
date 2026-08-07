fdf7d37ff45e5b87e6d589f8830e5a2e17f31f7a
pvlc <71274828+pvlc@users.noreply.github.com>
2026-08-07 13:23:48 +0800

    Revert "feat(ui): add backfill-image-urls button to settings"
    This reverts commit 63557e1b9a3c24f01b7b76c648c7efc6be9b2dbe.

--- mail-vue/src/request/setting.js
+++ mail-vue/src/request/setting.js
@@ -27,7 +27,3 @@ export function setBlackList(params) {
 export function migrateAttachments(params) {
     return http.post('/setting/migrateAttachments', params)
 }
-
-export function backfillImageUrls(params) {
-    return http.post('/setting/backfillImageUrls', params)
-}
